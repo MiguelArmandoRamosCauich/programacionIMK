580c7737fa5681dbd60a9976df0e5179e253651f
MiguelArmandoRamosCauich <15222539@modelo.edu.mx>
2022-10-26 12:36:51 -0500

    Examen Parcial 2

---
+++ 0x0A_examenParcial2/1-main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main()
+{
+    int N;
+    int i;
+    int j;
+    printf("Ingrese el numero de lineas:\n");
+    scanf("%d", &N);
+
+    
+
+    for(i=1; i<=N; i++)
+    {
+        for(j=1; j<=i; j++)
+        {
+            printf(" ");
+        }
+
+        for(j=(2*i-2); j<(2*N-1); j++)
+        {
+            printf("*");
+        }
+
+        for(j=1; j<=i; j++)
+        {
+            printf(" ");
+        }
+
+        printf("\n");
+    }
+    return (0);
+}
